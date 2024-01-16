f7321825c0208bc24472b01f2254faf1356d295f
PapiMatteo <matteopapi.work@gmail.com>
2024-01-16 15:41:28 +0100

    add: controller and visualization

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Welcome to laravel</h1>
+    <ul>
+        @foreach ($trains as $train)
+            <li>
+                <h3>Treno numero {{ $train->train_code }}</h3>
+                <p>{{ $train->date }}</p>
+                <p>In partenza da {{ $train->departure_station }} delle {{ $train->departure_time }}</p>
+                <p>Diretto a {{ $train->arrive_station }}</p>
+            </li>
+        @endforeach
+    </ul>
 @endsection
